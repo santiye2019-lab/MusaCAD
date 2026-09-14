fb7a72451710ada6852ad78dec575ee16efb3ee4
santiye2019-lab <santiye2019@gmail.com>
2026-09-14 22:54:00 +0300

    Test AutoCAD TrueColor method-bit masking

--- tests/DxfColorTest.java
+++ tests/DxfColorTest.java
@@ -2,18 +2,22 @@ import com.musa.cad.DxfColor;
 import java.util.*;
 
 public class DxfColorTest {
+    private static void eq(int actual,int expected,String name){if(actual!=expected)throw new AssertionError(name+": 0x"+Integer.toHexString(actual));}
     public static void main(String[] args){
-        if(DxfColor.aciArgb(1)!=0xFFFF0000)throw new AssertionError("ACI red");
-        if(DxfColor.aciArgb(2)!=0xFFFFFF00)throw new AssertionError("ACI yellow");
-        if(DxfColor.aciArgb(3)!=0xFF00FF00)throw new AssertionError("ACI green");
-        if(DxfColor.aciArgb(5)!=0xFF0000FF)throw new AssertionError("ACI blue");
-        if(DxfColor.aciArgb(7)!=0xFFFFFFFF)throw new AssertionError("ACI 7 white");
-        if(DxfColor.aciArgb(253)!=0xFF828282)throw new AssertionError("ACI 253 standard gray");
-        Map<String,Integer> layers=new HashMap<>();layers.put("BORU",0xFF123456);
-        if(DxfColor.argb(DxfColor.resolve(256,-1,"boru",null),layers)!=0xFF123456)throw new AssertionError("BYLAYER");
+        eq(DxfColor.aciArgb(1),0xffff0000,"ACI red");
+        eq(DxfColor.aciArgb(2),0xffffff00,"ACI yellow");
+        eq(DxfColor.aciArgb(3),0xff00ff00,"ACI green");
+        eq(DxfColor.aciArgb(4),0xff00ffff,"ACI cyan");
+        eq(DxfColor.aciArgb(5),0xff0000ff,"ACI blue");
+        eq(DxfColor.aciArgb(6),0xffff00ff,"ACI magenta");
+        eq(DxfColor.aciArgb(11),0xffffaaaa,"ACI pastel red");
+        eq(DxfColor.trueColor(3261100864L),0x00607340,"TrueColor method bits");
+        DxfColor.Ref trueColor=DxfColor.resolve(256,0x123456,"X",null);
+        eq(DxfColor.argb(trueColor,Collections.emptyMap()),0xff123456,"TrueColor");
+        Map<String,Integer> layers=new HashMap<>();layers.put("BORU",0xff00ff00);
+        eq(DxfColor.argb(DxfColor.resolve(256,-1,"boru",null),layers),0xff00ff00,"BYLAYER");
         DxfColor.Ref parent=DxfColor.resolve(1,-1,"0",null);
-        if(DxfColor.argb(DxfColor.resolve(0,-1,"0",parent),layers)!=0xFFFF0000)throw new AssertionError("BYBLOCK");
-        if(DxfColor.argb(DxfColor.resolve(256,0x336699,"0",null),layers)!=0xFF336699)throw new AssertionError("TrueColor");
-        System.out.println("9 DXF color cases passed");
+        eq(DxfColor.argb(DxfColor.resolve(0,-1,"0",parent),layers),0xffff0000,"BYBLOCK");
+        System.out.println("11 DXF color cases passed");
     }
 }
