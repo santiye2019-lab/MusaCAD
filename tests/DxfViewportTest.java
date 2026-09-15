06c72eafe8c0314cc5bdba8770959dce58fc0405
santiye2019-lab <santiye2019@gmail.com>
2026-09-15 23:40:34 +0300

    Test oblique viewport projection and targets

--- tests/DxfViewportTest.java
+++ tests/DxfViewportTest.java
@@ -15,9 +15,24 @@ public class DxfViewportTest {
         DxfViewport.Spec rotated=DxfViewport.of(0,0,100,100,0,0,100,Math.PI/2,3,1,0,0,1);
         p=DxfViewport.point(rotated,10,0);close(p[0],0);close(p[1],-10);
 
+        DxfViewport.Spec targeted=DxfViewport.of(25,30,100,80,0,0,40,0,4,1,0,0,1,5,7,0);
+        p=DxfViewport.point(targeted,5,7);close(p[0],25);close(p[1],30);
+        p=DxfViewport.point(targeted,15,7);close(p[0],45);close(p[1],30);
+
+        DxfViewport.Spec oblique=DxfViewport.of(0,0,100,100,0,0,100,0,5,1,1,1,1,0,0,0);
+        if(!oblique.supported())throw new AssertionError("oblique viewport");
+        p=DxfViewport.point(oblique,1,0);close(p[0],-1/Math.sqrt(2));close(p[1],-1/Math.sqrt(6));
+        p=DxfViewport.point(oblique,0,1);close(p[0],1/Math.sqrt(2));close(p[1],-1/Math.sqrt(6));
+
+        DxfViewport.Spec obliqueTarget=DxfViewport.of(10,20,100,100,0,0,50,0,6,1,1,1,1,3,4,5);
+        double rx=-1/Math.sqrt(2),ry=1/Math.sqrt(2),ux=-1/Math.sqrt(6),uy=-1/Math.sqrt(6),uz=2/Math.sqrt(6);
+        double u=3*rx+4*ry, vv=3*ux+4*uy+5*uz;
+        p=DxfViewport.point(obliqueTarget,3,4);close(p[0],10-2*u);close(p[1],20-2*vv);
+
         if(DxfViewport.of(0,0,10,10,0,0,10,0,1,1,0,0,1).supported())throw new AssertionError("overall paper viewport");
         if(DxfViewport.of(0,0,10,10,0,0,10,0,2,0,0,0,1).supported())throw new AssertionError("off viewport");
-        if(DxfViewport.of(0,0,10,10,0,0,10,0,2,1,1,0,1).supported())throw new AssertionError("3D view not yet 2D-safe");
-        System.out.println("14 DXF viewport geometry and clipping cases passed");
+        if(DxfViewport.of(0,0,10,10,0,0,10,0,2,1,1,0,0).supported())throw new AssertionError("pure side view");
+        if(DxfViewport.of(0,0,10,10,0,0,10,0,2,1,0,0,0).supported())throw new AssertionError("zero direction");
+        System.out.println("24 DXF viewport geometry and clipping cases passed");
     }
 }
