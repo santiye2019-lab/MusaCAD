e8392e2c6d32c0c9c1892b846a5de0ba9ed52f73
santiye2019-lab <santiye2019@gmail.com>
2026-09-16 13:34:47 +0300

    Test ATTDEF template visibility

--- tests/DxfVisibilityTest.java
+++ tests/DxfVisibilityTest.java
@@ -7,9 +7,13 @@ public class DxfVisibilityTest {
         yes(DxfVisibility.invisible("LINE",1,0),"common hidden");
         no(DxfVisibility.invisible("LINE",0,1),"LINE group70 is not attribute visibility");
         yes(DxfVisibility.invisible("ATTRIB",0,1),"invisible ATTRIB");
-        yes(DxfVisibility.invisible("attdef",0,5),"invisible ATTDEF plus other flags");
-        no(DxfVisibility.invisible("ATTRIB",0,2),"constant visible ATTRIB");
-        no(DxfVisibility.invisible("ATTDEF",0,8),"preset visible ATTDEF");
-        System.out.println("6 DXF entity visibility cases passed");
+        no(DxfVisibility.invisible("ATTRIB",0,2),"ATTRIB flag 2 does not hide reference");
+        yes(DxfVisibility.invisible("ATTDEF",0,1),"invisible ATTDEF");
+        yes(DxfVisibility.invisible("ATTDEF",0,0),"variable ATTDEF template is not drawn");
+        yes(DxfVisibility.invisible("attdef",0,8),"preset variable ATTDEF template is not drawn");
+        no(DxfVisibility.invisible("ATTDEF",0,2),"constant ATTDEF remains visible");
+        no(DxfVisibility.invisible("ATTDEF",0,10),"constant preset ATTDEF remains visible");
+        yes(DxfVisibility.invisible("ATTDEF",0,3),"invisible constant ATTDEF stays hidden");
+        System.out.println("10 DXF entity visibility cases passed");
     }
 }
