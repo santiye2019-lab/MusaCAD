b17f0e34c71f3b59446db4c3d5f1a9d9b2eeb535
santiye2019-lab <santiye2019@gmail.com>
2026-09-18 21:57:38 +0300

    Update command test after unsupported removal

--- tests/CadCommandTest.java
+++ tests/CadCommandTest.java
@@ -5,7 +5,7 @@ public class CadCommandTest {
         String[] commands={"L","PL","C","A","EL","PO","XL","REC","TEXT","SEL","P","M","CO","RO","E","SC","MI","O","AR","X","OS","RE","TR","EX","F","CHA","BR","PE","LI","MA","J","H","S","B","I","D","DLI","DAL","LA","PR","DI","AA","ZE","Z","U","REDO","QS","?"};
         for(String command:commands){
             CadCommand.Action action=CadCommand.parse(command);
-            if(action==CadCommand.Action.NONE||action==CadCommand.Action.UNSUPPORTED)throw new AssertionError(command+" not implemented: "+action);
+            if(action==CadCommand.Action.NONE)throw new AssertionError(command+" not implemented: "+action);
         }
         if(CadCommand.parse("nonsense")!=CadCommand.Action.NONE)throw new AssertionError("unknown command");
         if(CadCommand.parse("S")!=CadCommand.Action.STRETCH)throw new AssertionError("S must be STRETCH");
