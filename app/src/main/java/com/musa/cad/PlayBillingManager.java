5ca26df201e8957b44e29bc674e33f7690e3ea66
santiye2019-lab <santiye2019@gmail.com>
2026-09-24 23:59:03 +0300

    Fix Billing 9 pending purchase initialization

--- app/src/main/java/com/musa/cad/PlayBillingManager.java
+++ app/src/main/java/com/musa/cad/PlayBillingManager.java
@@ -12,6 +12,7 @@ import com.android.billingclient.api.BillingFlowParams;
 import com.android.billingclient.api.BillingResult;
 import com.android.billingclient.api.ProductDetails;
 import com.android.billingclient.api.Purchase;
+import com.android.billingclient.api.PendingPurchasesParams;
 import com.android.billingclient.api.PurchasesUpdatedListener;
 import com.android.billingclient.api.QueryProductDetailsParams;
 import com.android.billingclient.api.QueryPurchasesParams;
@@ -40,7 +41,7 @@ public final class PlayBillingManager implements PurchasesUpdatedListener, Billi
         this.listener=listener;
         billingClient=BillingClient.newBuilder(this.context)
             .setListener(this)
-            .enablePendingPurchases()
+            .enablePendingPurchases(PendingPurchasesParams.newBuilder().enableOneTimeProducts().build())
             .enableAutoServiceReconnection()
             .build();
     }
